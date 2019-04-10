99445ce038fad734fc4e50bc2ddddb766fc99b48
fanyx <fanyx@shanghaitech.edu.cn>
2019-04-11 00:09:17 +0800

    functional poly_func pass

--- src/polynomial.cpp
+++ src/polynomial.cpp
@@ -1,3 +1,201 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+
+#include <cmath>
 #include "polynomial.hpp"
+
+Polynomial::Polynomial() {
+
+}
+
+Polynomial::Polynomial( const std::vector<float> & coeffs ) {
+  _coeffs = coeffs;
+}
+
+Polynomial::Polynomial( std::vector<float> && coeffs ) {
+  _coeffs = std::move( coeffs );
+}
+
+Polynomial::Polynomial( std::initializer_list<float> coeffs ) {
+  _coeffs = std::vector<float>(coeffs.begin(), coeffs.end());
+}
+
+Polynomial::Polynomial( const std::string & path ) {
+  std::ifstream file(path);
+  _coeffs = std::vector<float>();
+  std::string coeff_str;
+  //std::string vector_str = std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+  
+  while( getline(file, coeff_str, ' ') ) {
+    float coeff = stof(coeff_str);
+    _coeffs.push_back(coeff);
+  }
+  //std::cout<<"QAQ "<<vector_str<<std::endl;
+}
+
+Polynomial::Polynomial( const Polynomial & p) {
+  *this = p;
+}
+
+Polynomial::Polynomial( Polynomial && p) {
+  *this = std::move(p);
+}
+
+Polynomial & Polynomial::operator=( const Polynomial & p ) {
+  _coeffs = p._coeffs;
+  return *this;
+}
+
+Polynomial & Polynomial::operator=( Polynomial && p ) {
+  *this = std::move(p);
+  return *this;
+}
+
+Polynomial::~Polynomial(void) {
+
+}
+
+float & Polynomial::operator[]( int index ) {
+  return _coeffs[ index ];
+}
+
+const float & Polynomial::operator[]( int index ) const {
+  return _coeffs[ index ];
+}
+
+int Polynomial::size() const {
+  return _coeffs.size();
+}
+
+void Polynomial::print() const {
+  for( auto i : _coeffs ) {
+    std::cout<< i <<" "; 
+  }
+  std::cout<<std::endl;
+}
+
+Polynomial Polynomial::operator+( const Polynomial & p ) const {
+  std::vector<float> ret = std::vector<float>();
+  if ( size() >= p.size() ) {
+    for ( int i = 0; i < p.size(); i++ ) {
+      ret.push_back( _coeffs[i] + p._coeffs[i]);
+    }
+    ret.insert(ret.end(), _coeffs.begin()+p.size(), _coeffs.end());
+  }else {
+    for ( int i = 0; i < size(); i++ ) {
+      ret.push_back( _coeffs[i] + p._coeffs[i]);
+    }
+    ret.insert(ret.end(), p._coeffs.begin()+size(), p._coeffs.end());
+  }
+  return Polynomial(ret);
+}
+
+Polynomial & Polynomial::operator+=( const Polynomial & p ) {
+  if ( size() >= p.size() ) {
+    for ( int i = 0; i < p.size(); i++ ) {
+      _coeffs[i] += p._coeffs[i];
+    }
+  }else {
+    for ( int i = 0; i < size(); i++ ) {
+      _coeffs[i] += p._coeffs[i];
+    }
+    for ( int i = size(); i < p.size(); i++ ) {
+      _coeffs.push_back(p._coeffs[i]);
+    }
+  }
+  return *this;
+}
+
+Polynomial Polynomial::operator-( const Polynomial & p ) const {
+  std::vector<float> ret = std::vector<float>();
+  if ( size() >= p.size() ) {
+    for ( int i = 0; i < p.size(); i++ ) {
+      ret.push_back( _coeffs[i] - p._coeffs[i]);
+    }
+    ret.insert(ret.end(), _coeffs.begin()+p.size(), _coeffs.end());
+  }else {
+    for ( int i = 0; i < size(); i++ ) {
+      ret.push_back( _coeffs[i] - p._coeffs[i]);
+    }
+    for ( int i = size(); i < p.size(); i++ ) {
+      ret.push_back( -p._coeffs[i]);
+    }
+  }
+  return Polynomial(ret);
+}
+
+Polynomial & Polynomial::operator-=( const Polynomial & p ) {
+  if ( size() >= p.size() ) {
+    for ( int i = 0; i < p.size(); i++ ) {
+      _coeffs[i] -= p._coeffs[i];
+    }
+  }else {
+    for ( int i = 0; i < size(); i++ ) {
+      _coeffs[i] -= p._coeffs[i];
+    }
+    for ( int i = size(); i < p.size(); i++ ) {
+      _coeffs.push_back(-p._coeffs[i]);
+    }
+  }
+  return *this;
+}
+
+Polynomial Polynomial::operator*( const Polynomial & p ) const {
+  std::vector<float> ret = std::vector<float>(size()+p.size()-1, 0.0);
+  for ( int i = 0; i < size(); i ++ ) {
+    for ( int j = 0; j < p.size(); j++ ) {
+      ret[i+j] += _coeffs[i]*p._coeffs[j];
+    }
+  }
+  return Polynomial(ret);
+}
+
+Polynomial & Polynomial::operator*=( const Polynomial & p ) {
+  std::vector<float> ret = std::vector<float>(size()+p.size()-1, 0.0);
+  for ( int i = 0; i < size(); i ++ ) {
+    for ( int j = 0; j < p.size(); j++ ) {
+      ret[i+j] += _coeffs[i]*p._coeffs[j];
+    }
+  }
+  _coeffs = std::move(ret);
+  return *this;
+}
+
+Polynomial Polynomial::operator*( float factor ) const {
+  std::vector<float> ret = std::vector<float>();
+  for ( auto i : _coeffs ) {
+    ret.push_back( i * factor );
+  }
+  return Polynomial(ret);
+}
+
+Polynomial Polynomial::compose( const Polynomial & p ) const {
+  std::vector<float> ret;
+  //exit(0);
+  Polynomial p_pow_i = Polynomial(p);
+  Polynomial p_r = Polynomial(std::vector<float>{_coeffs[0]});
+  for ( int i =1; i < _coeffs.size(); i++ ) {
+    p_r += (p_pow_i * _coeffs[i]) ;
+    p_pow_i *= p;
+  }
+  // std::cout << "COM ";
+  // for ( auto i : p_r._coeffs){
+  //   std::cout << i << " ";
+  // }
+  // std::cout << std::endl;
+
+  return p_r;
+}
+
+float Polynomial::operator()( float x ) const {
+  float result = 0.0;
+  for ( int i = 0; i < size(); i++ ) {
+    result += _coeffs[i]*(pow(x,i));
+  }
+  //std::cout<< result<<std::endl;
+  return result;
+}
+
 
 //TODO: implement this file
--- src/polynomial_parser.cpp
+++ src/polynomial_parser.cpp
@@ -1,3 +1,152 @@
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+
 #include "polynomial_parser.hpp"
 
+Polynomial PolynomialParser::compute_polynomial( const std::string & filename,
+                                                 std::map<std::string, Polynomial> & polys ) {
+  std::ifstream file(filename);
+  std::istreambuf_iterator<char> it(file),end;
+  std::vector<std::string> stack_expr_str = std::vector<std::string>();
+  std::string pol_str = std::string();
+  while(it != end) {
+    if ( *it == '(' ||
+        * it == ')' ||
+        * it == '+' ||
+        * it == '-' ||
+        * it == '*') {
+      if ( pol_str.size() != 0 ) {
+        stack_expr_str.push_back( pol_str );
+        pol_str.clear();
+      }
+      stack_expr_str.push_back(std::string(1, *it));
+    }else {
+      pol_str.append(1, *it);
+    }
+    it++;
+  }
+
+  std::cout <<"QBQ ";
+  for ( auto i : stack_expr_str ) {
+    std::cout << i << " ";
+  }
+  std::cout<<std::endl;
+  std::reverse(stack_expr_str.begin(), stack_expr_str.end());
+  std::cout <<"QBQ ";
+  for ( auto i : stack_expr_str ) {
+    std::cout << i << " ";
+  }
+  std::cout<<std::endl;
+  //exit(0);
+  std::vector<std::string> stack_stack = std::vector<std::string>();
+  while ( !stack_expr_str.empty()) {
+    std::cout << "QAQ ";for ( auto i : stack_expr_str) { std::cout << i << " ";} std::cout<<std::endl;
+    std::cout << "QZQ ";for ( auto i : stack_stack) { std::cout << i << " ";}std::cout<<std::endl;
+    if (stack_expr_str.back() == ")" ) {
+      std::vector<std::string>::reverse_iterator it = stack_stack.rbegin();
+      it++;
+      // Case (p) with stack: ...(p
+      //     with expr_stack: ...)
+      if ( *it == "(" ) {
+        // Pop ) from expr
+        stack_expr_str.pop_back();
+
+        std::string p2_str = stack_stack.back();
+        // Pop p from stack
+        stack_stack.pop_back();
+        // Pop ( from stack
+        stack_stack.pop_back();
+        // Case p1(p2) with stack: ...p1 
+        if ( stack_stack.empty() ) {
+          stack_stack.push_back(p2_str);
+        }else if ( stack_stack.back().size() > 1 ) {
+          std::string p1_str = stack_stack.back();
+          Polynomial p1 = polys[p1_str];
+          Polynomial p2 = polys[p2_str];
+          std::string p3_str = p1_str + p2_str;
+          Polynomial p3 = p1.compose(p2);
+          polys[p3_str] = p3;
+          // Pop p1 from stack
+          stack_stack.pop_back();
+          // Push p3 into stack
+          stack_stack.push_back(p3_str);
+        }else {
+          stack_stack.push_back(p2_str);
+        }
+      }
+    }else if (stack_expr_str.back().size() > 1){
+      // Case p1 op p2 with stack: ...p1 op
+      //                     expr: ...p2
+      if ( stack_stack.back() == "+" ) {
+        // Pop + from stack
+        stack_stack.pop_back();
+        std::string p1_str = stack_stack.back();
+        Polynomial p1 = polys[p1_str];
+        // Pop p1 from stack
+        stack_stack.pop_back();
+        std::string p2_str = stack_expr_str.back();
+        Polynomial p2 = polys[p2_str];
+        // Pop p2 from expr
+        stack_expr_str.pop_back();
+        std::string p3_str = p1_str + "+" + p2_str;
+        Polynomial p3 = p1 + p2;
+        polys[p3_str] = p3;
+        // Push p3 into stack
+        stack_stack.push_back(p3_str);
+      }else if ( stack_stack.back() == "-" ) {
+        // Pop + from stack
+        stack_stack.pop_back();
+        std::string p1_str = stack_stack.back();
+        Polynomial p1 = polys[p1_str];
+        // Pop p1 from stack
+        stack_stack.pop_back();
+        std::string p2_str = stack_expr_str.back();
+        Polynomial p2 = polys[p2_str];
+        // Pop p2 from expr
+        stack_expr_str.pop_back();
+        std::string p3_str = p1_str + "-" + p2_str;
+        Polynomial p3 = p1 - p2;
+        polys[p3_str] = p3;
+        // Push p3 into stack
+        stack_stack.push_back(p3_str);
+      }else if ( stack_stack.back() == "*" ) {
+        // Pop + from stack
+        stack_stack.pop_back();
+        std::string p1_str = stack_stack.back();
+        Polynomial p1 = polys[p1_str];
+        // Pop p1 from stack
+        stack_stack.pop_back();
+        std::string p2_str = stack_expr_str.back();
+        Polynomial p2 = polys[p2_str];
+        // Pop p2 from expr
+        stack_expr_str.pop_back();
+        std::string p3_str = p1_str + "*" + p2_str;
+        Polynomial p3 = p1 * p2;
+        polys[p3_str] = p3;
+        // Push p3 into stack
+        stack_stack.push_back(p3_str);
+      }else {
+        stack_stack.push_back( stack_expr_str.back());
+        stack_expr_str.pop_back();
+      }      
+    }else {
+      stack_stack.push_back( stack_expr_str.back());
+      stack_expr_str.pop_back();
+    }
+    
+  }
+
+//   exit(0);
+  
+  Polynomial ret = polys[stack_stack.front()];
+
+  return ret;
+
+}
+
+std::function<float(float)> PolynomialParser::compute_lambda( const std::string & filename, std::map<std::string, Polynomial> & polys) {
+  scalarFct s;
+  return s;
+}
 //TODO: implement this file
